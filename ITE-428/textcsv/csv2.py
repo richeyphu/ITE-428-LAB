a9c9534e6df25974d214986d2653e901c0642472
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2021-07-14 12:00:59 +0700

    Add files via upload

--- ITE-428/textcsv/csv2.py
+++ ITE-428/textcsv/csv2.py
@@ -1,3 +1,6 @@
+import csv
+
+
 def readFile(FN):
     with open(file=FN, mode='r', encoding='utf8') as fn:
         for i in fn:
@@ -14,7 +17,35 @@ def readFile2(FN):
         print("Stript   [{}]".format(data[0].strip('\n')))
 
 
+def writecsv(data, fn):
+    with open(file=fn, mode='a', encoding='utf-8', newline="") as f:
+        fw = csv.writer(f, delimiter=",")
+        fw.writerows(data)
+
+
+def readcsv(fn):
+    with open(file=fn,mode='r',encoding='utf-8', newline="") as fr:
+        fr = csv.reader(fr)
+        for i in fr:
+            print("Product : {}".format(i))
+
+
 if __name__ == '__main__':
-    filename = "myfile/products.csv"
+    filename = "myFile/products.csv"
     # readFile(filename)
-    readFile2(filename)
+    # readFile2(filename)
+    readcsv(filename)
+
+    # Ref Slide 276, 280
+    # No = int(input("Enter Number of New Product : "))
+    # products = []
+    # for i in range(No):
+    #     product = []
+    #     print("\nProduct Number [{}]".format(i + 1))
+    #     print("=" * 20)
+    #     product.append(input("Enter product Name : "))
+    #     product.append(input("Enter product Price : "))
+    #     product.append(input("Enter product Stock : "))
+    #     products.append(product)
+    # print("{}".format(products))
+    # writecsv(products, filename)
---
+++ ITE-428/textcsv/fire.py
@@ -0,0 +1,59 @@
+import csv
+
+
+def readcsv(fn):
+    with open(file=fn, mode='r', encoding='utf-8', newline="") as f:
+        fr = csv.reader(f)
+        next(fr)
+
+        incidents = []
+        morefire = []
+        alwaysd = []
+        lessinj = []
+        for i in fr:
+            # print("{}".format(i))
+            month = i[1]
+            f55, f56, f57 = int(i[2]), int(i[3]), int(i[4])
+            d55, d56, d57 = int(i[5]), int(i[6]), int(i[7])
+            i55, i56, i57 = int(i[8]), int(i[9]), int(i[10])
+            mean = (f55 + f56 + f57) / 3
+            incidents.append([month, mean])
+            if f55 < f56 < f57:
+                morefire.append(month)
+            if d55 > 0 and d56 > 0 and d57 > 0:
+                alwaysd.append(month)
+            if i55 > i56 > i57:
+                lessinj.append(month)
+
+        for i, v in enumerate(sorted(incidents, reverse=True, key=lambda x: x[1])):
+            print("{}.) {} จำนวน {:.2f} ครั้ง".format(i + 1, v[0], v[1]))
+        line()
+
+        print("เดือนที่ต้องระวังเนื่องจากมีเหตุการณ์ไฟไหม้มากขึ้นทุกปีที่ผ่านมา คือ", end="")
+        for i, v in enumerate(morefire):
+            print(" {}".format(v), end=(" ," if i < len(morefire) - 1 else "\n"))
+        line()
+
+        print("เดือนที่มีคนตายทุกปี คือ", end="")
+        for i, v in enumerate(alwaysd):
+            print(" {}".format(v), end=(" ," if i < len(alwaysd) - 1 else "\n"))
+        line()
+
+        print("เดือนที่เจ็บลดลงทุกปี คือ", end="")
+        for i, v in enumerate(lessinj):
+            print(" {}".format(v), end=(" ," if i < len(lessinj) - 1 else "\n"))
+        line()
+
+
+def line():
+    print("-" * 85)
+
+
+if __name__ == '__main__':
+    filename = "myFile/fire.csv"
+
+    line()
+    print("\tค่าเฉลี่ย การเกิดเหตุการณ์เพลิงไหม้ ปี 2555-2557")
+    line()
+
+    readcsv(filename)
