41dde7490b970a0a69076b6dfc513d752db65f5f
AkaraSellegg <55230837+richeyphu@users.noreply.github.com>
2021-07-31 22:20:18 +0700

    Add files via upload

---
+++ ITE-428/cloudMongodb/mongodb1.py
@@ -0,0 +1,100 @@
+import pymongo
+from datetime import datetime
+import socket
+import dns
+import csv
+
+
+def insertData():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('Tni')
+        # db.students.insert_one({})
+        # db.students.insert_many([{},{},...])
+
+        stu_id = "1913110027"
+        name = "Phurit"
+        lastname = "D."
+        phy_data = {'weight': 70, 'height': 170}
+        major = "IT"
+        year = 3
+        gpax = 3.97
+        ip_addr = socket.gethostbyname(socket.gethostname())
+        db.students.insert_one({'student_id': stu_id,
+                                'name': name,
+                                'lastname': lastname,
+                                'physical_data': phy_data,
+                                'major': major,
+                                'year': year,
+                                'gpax': gpax,
+                                'from': ip_addr,
+                                'updated': datetime.now()
+                                })
+
+        print("Insert Success")
+
+
+def bmi():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_default_database()
+        condition = {}
+        cursor = db.students.find(condition)
+        found = db.students.count_documents(condition)
+        print("พบ = {} รายการ\n".format(found))
+        for i in cursor:
+            bmi = getBMI(i['physical_data']['weight'], i['physical_data']['height'])
+            print("{} {} (BMI={:.2f})".format(i['name'], i['lastname'], bmi))
+
+
+def getBMI(w, h):
+    return float(w) / (float(h) / 100) ** 2
+
+
+def useStudents():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_default_database()
+        condition = {'major': 'IT'}
+        cursor = db.students.find(condition)
+        found = db.students.count_documents(condition)
+        print("พบ = {} รายการ\n".format(found))
+        for i in cursor:
+            # print("{}".format(i))
+            print("NAME : {}".format(i['name']))
+
+
+def export_students():
+    currentdate = str(datetime.now())
+    currentdate = currentdate.split('.')[0]
+    currentdate = currentdate.replace(':', '-')
+    with (open(file="Exportdata/students_{}.csv".format(currentdate), mode='w', encoding='utf8', newline="")) as f:
+        fw = csv.writer(f)
+        fw.writerow(['ID', 'NAME', 'LASTNAME', 'GPAX', 'BMI'])
+        fw.writerows(getQueryList())
+    print("Export Success on {}".format(currentdate))
+
+
+def getQueryList():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_default_database()
+        condition = {}
+        cursor = db.students.find(condition)
+        data = []
+        for i in cursor:
+            bmi = getBMI(i['physical_data']['weight'], i['physical_data']['height'])
+            data.append([i['student_id'], i['name'], i['lastname'], i['gpax'], round(bmi, 2)])
+        return data
+
+
+if __name__ == '__main__':
+    # cloudDatabase = "mongodb+srv://test:<password>@cluster0.hf3dm.mongodb.net/Tni?retryWrites=true&w=majority"
+    cloudDatabase = "mongodb+srv://Phurit:<password>@cluster0.rqhdl.mongodb.net/Tni?retryWrites=true&w=majority"
+
+    # hostname = socket.gethostname()
+    # ip_address = socket.gethostbyname(hostname)
+    # ip_address = socket.gethostbyname_ex(hostname)
+    # print("{} -- {}".format(hostname, ip_address))
+    # print("{}".format(datetime.now()))
+
+    insertData()
+    # useStudents()
+    # bmi()
+    # export_students()
---
+++ ITE-428/cloudMongodb/mongodb2.py
@@ -0,0 +1,160 @@
+import pymongo
+import dns
+import csv
+from datetime import datetime
+
+
+# update set column=? where ...
+def updateData():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_default_database()
+        # db = conn.get_database('')
+        # update_one(where , set)     update_many(where , set)
+        where = {'student_id': '1913110027'}
+        setTo = {'$set': {'major': 'DC'}}
+        res = db.students.update_one(where, setTo)
+        print("{} -- {}".format(res.matched_count, res.modified_count))
+
+
+def deleteData():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('Tni')
+        # .delete_one(where) --> ลบตัวแรกที่เจอ  , .delete_many(where) --> ลบทุกตัวที่เจอ
+        where = {'student_id': '21013909091'}
+        res = db.students.delete_one(where)
+        print("{}".format(res.deleted_count))
+
+
+# sample_airbnb
+def cleaning_fee():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_airbnb')
+        # number_of_reviews>150 และมี cleaning_fee ด้วย
+        con1 = {'number_of_reviews': {'$gt': 150}}
+        con2 = {'cleaning_fee': {'$exists': True}}
+        where = {'$and': [con1, con2]}
+        # where = {'$and': [{'number_of_reviews': {'$gt': 150}}, {'cleaning_fee': {'$exists': True}}]}
+        cursor = db.listingsAndReviews.find(where)
+        found = db.listingsAndReviews.count_documents(where)
+        print("พบข้อมูล {:,}".format(found))
+        no = 1
+        for i in cursor:
+            print("{}.) {} -- {}".format(no, i['name'], i['listing_url']))
+            print("{}".format(i['cleaning_fee']))
+            no += 1
+
+
+def airbnb_no_of_reviews():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_airbnb')
+        where = {}
+        cursor = db.listingsAndReviews.find(where)
+
+        line()
+        for i in cursor:
+            print("NAME = {}".format(i['name']))
+            print("TOTAL COMMENT : {}".format(i['number_of_reviews']))
+            # print("TOTAL COMMENT : {}".format(len(i['reviews'])))
+            line()
+
+
+def see_reviews():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_airbnb')
+        name = input("Please Enter AirBnb Name : ")
+        where = {'name': {"$regex": '^' + name + '$', "$options": "i"}}
+        # where = {'name': name}
+        cursor = db.listingsAndReviews.find(where)
+        found = db.listingsAndReviews.count_documents(where)
+        if found:
+            line()
+            print("NAME : {}".format(cursor[0]['name']))
+            print("TOTAL {} COMMENT(S)".format(cursor[0]['number_of_reviews']))
+            line()
+            for i, v in enumerate(cursor[0]['reviews']):
+                print(" {}.) {}\n".format(i + 1, v['comments']))
+        else:
+            print("No Data Found")
+
+
+def export_air_bnb_location():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_airbnb')
+        where = {'address.location.is_location_exact': True}
+        cursor = db.listingsAndReviews.find(where)
+
+    with (open(file="Exportdata/data_air_bnb.csv", mode='w', encoding='utf8', newline="")) as f:
+        fw = csv.writer(f)
+        fw.writerow(['Place', 'Country', 'Latitude', 'Longitude'])
+        for i in cursor:
+            country = i['address']['country']
+            latitude = i['address']['location']['coordinates'][1]
+            longitude = i['address']['location']['coordinates'][0]
+            data = [i['name'], country, latitude, longitude]
+            fw.writerow(data)
+
+    print("Export Success")
+
+
+# sample_restaurants
+def show_average_Score():
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_restaurants')
+        name = input("กรอก restaurant_id ที่ต้องการดูคะแนนเฉลี่ย : ")
+        where = {'restaurant_id': name}
+        found = db.restaurants.count_documents(where)
+        if found:
+            cursor = db.restaurants.find(where)
+            print("{}\n".format(cursor[0]['name']))
+            sum = 0
+            for i, v in enumerate(cursor[0]['grades']):
+                sum += v['score']
+                print("Score {:>2} = {:>3}".format(i + 1, v['score']))
+            print("\nAVERAGE = {:.2f}".format(sum / (i + 1)))
+        else:
+            print("ไม่พบ restaurant_id")
+
+
+# sample_mflix
+def comment_mflix():
+    movie_name = input("หนังที่ต้องการ Comment : ")
+    with pymongo.MongoClient(cloudDatabase) as conn:
+        db = conn.get_database('sample_mflix')
+        where = {'title': {"$regex": '^' + movie_name + '$', "$options": "i"}}
+        found = db.movies.count_documents(where)
+        if found:
+            cursor = db.movies.find(where)
+            movie_id = cursor[0]['_id']
+            db.comments.insert_one({'name': "{} {}".format(input("ชื่อ : "), input("นามสกุล : ")),
+                                    'email': input("E-mail : "),
+                                    'movie_id': movie_id,
+                                    'text': input("Comment ว่า : "),
+                                    'date': datetime.now()
+                                    })
+            print("\nComment Success")
+        else:
+            print("ไม่พบหนังที่ต้องการ")
+
+
+def line():
+    print('-' * 50)
+
+
+if __name__ == '__main__':
+    # cloudDatabase = "mongodb+srv://test:<password>@cluster0.hf3dm.mongodb.net/Tni?retryWrites=true&w=majority"
+    cloudDatabase = "mongodb+srv://Phurit:<password>@cluster0.rqhdl.mongodb.net/Tni?retryWrites=true&w=majority"
+
+    # updateData()
+    # deleteData()
+
+    # sample_airbnb
+    # cleaning_fee()
+    # airbnb_no_of_reviews()
+    # see_reviews()
+    # export_air_bnb_location()
+
+    # sample_restaurants
+    # show_average_Score()
+
+    # sample_mflix
+    comment_mflix()
